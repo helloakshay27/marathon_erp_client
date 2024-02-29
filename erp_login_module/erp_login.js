d48fdae790ee5b52d7b5ebf6b08121d0ba9658b0
Akshay <helloakshay27@gmail.com>
2024-02-29 17:57:23 +0530

    latest Changes 29-02

--- erp_login_module/erp_login.js
+++ erp_login_module/erp_login.js
@@ -41,7 +41,7 @@
         event.preventDefault();
 
         // Redirect to a particular HTML page
-        window.location.href = "/componets/erp_sidebar.html";
+        window.location.href = "/erp_home_module/erp_home.html";
     });
 
     $("#forgetPasswordForm").submit(function(event) {
